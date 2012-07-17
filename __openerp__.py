1fabfaf040fd59759399d6c45ba78c13396a8839
Cristian Sebastian Rocha <crocha@kekkaishi.jjdiazarana.net>
2012-07-17 11:42:26 -0300

    El modulo de plan contable cambia de nombre a l10n_ar_chart_generic. Se actualizaron los archivos de autores.

--- __openerp__.py
+++ __openerp__.py
@@ -1,32 +1,37 @@
 # -*- coding: utf-8 -*-
 ##############################################################################
 #
-#    Copyright (C) 2011-2014 OpenERP - Team de Localización Argentina.
-#                  https://launchpad.net/~openerp-l10n-ar-localization
+# Copyright (C) 2012 OpenERP - Team de Localización Argentina.
+# https://launchpad.net/~openerp-l10n-ar-localization
 #
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
 #
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-
 {
-    'name': 'Argentina - Base para los Web Services del AFIP',
-    'version': '0.1',
-    'author': 'OpenERP - Team de Localización Argentina',
-    'category': 'Argentina/WebServices',
-    'website': 'https://launchpad.net/~openerp-l10n-ar-localization',
-    'license': 'GPL-3',
+    'name':     'Argentina - Base para los Web Services del AFIP',
+    'version':  '0.1',
+    'author':   'OpenERP - Team de Localización Argentina',
+    'category': 'Localization/Argentina',
+    'website':  'https://launchpad.net/~openerp-l10n-ar-localization',
+    'license':  'GPL-3',
     'description': """
+Configuración y API para acceder a las Web Services de la AFIP
+
+Incluye:
+ - Wizard para instalar los claves para acceder a las Web Services.
+ - API para realizar consultas en la Web Services desde OpenERP.
+
 El módulo l10n_ar_wsafip permite a OpenERP acceder a los servicios del AFIP a través de Web Services. Este módulo es un servicio para administradores y programadores, donde podrán configurar el servidor, la autentificación y además tendrán acceso a una API genérica en Python para utilizar los servicios AFIP.
 """,
     'depends': [
