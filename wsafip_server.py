84f4d6d86a345196b05a653c054664ccdac0b004
Cristian Sebastian Rocha <crocha@kekkaishi.jjdiazarana.net>
2013-02-20 12:57:44 -0300

    [FIX] "from osv import" must be replaced by "from openerp.osv import"

--- wsafip_server.py
+++ wsafip_server.py
@@ -18,7 +18,7 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
-from osv import fields, osv
+from openerp.osv import fields, osv
 
 class wsafip_server(osv.osv):
     _name = "wsafip.server"
