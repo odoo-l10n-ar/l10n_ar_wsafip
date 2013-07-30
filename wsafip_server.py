483ae5fa257955e7d7920a9c19906971d14ed2d2
Cristian Sebastian Rocha <crocha@kekkaishi.jjdiazarana.net>
2013-07-30 08:23:52 -0300

    REF: Move wizard to from chart to new module

--- wsafip_server.py
+++ wsafip_server.py
@@ -22,9 +22,21 @@ from openerp.osv import fields, osv
 
 class wsafip_server(osv.osv):
     _name = "wsafip.server"
+
+    def get_name(self, cr, uid, ids, context=None):
+        if isinstance(ids, (list, tuple)) and not len(ids):
+            return []
+        if isinstance(ids, (long, int)):
+            ids = [ids]
+        reads = self.read(cr, uid, ids, ['name','class'], context=context)
+        res = []
+        for record in reads:
+            res.append(record['id']: "{name} [{class}]".format(record))
+        return res
+ 
     _columns = {
-        'name': fields.char('Name', size=64),
-        'code': fields.char('Code', size=16),
+        'name': fields.char('Name', size=64, select=1),
+        'code': fields.char('Code', size=16, select=1),
         'class': fields.selection([('production','Production'),('homologation','Homologation')], 'Class'),
         'url': fields.char('URL', size=512),
     }
