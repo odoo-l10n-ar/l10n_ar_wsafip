0e94de4738fcd8eefbd664eed3e591b51395e032
Cristian S. Rocha <cristian.rocha@moldeo.coop>
2015-11-19 16:45:31 -0300

    [FIX] fix other afip.wsfe_error model

--- addons/l10n_ar_wsafip_fe/invoice.py
+++ addons/l10n_ar_wsafip_fe/invoice.py
@@ -31,7 +31,8 @@ class account_invoice(models.Model):
     afip_batch_number = fields.Integer('AFIP Batch number', readonly=True)
     afip_cae = fields.Char('CAE number', size=24)
     afip_cae_due = fields.Date('CAE due')
-    afip_error_id = fields.Many2one('afip.wsfe_error', 'AFIP Status',
+    afip_error_id = fields.Many2one('wsafip.error',
+                                    string='Web Service Status',
                                     readonly=True)
     state = fields.Selection(selection_add=[('batch_validation',
                                              'Batch Validation')])
