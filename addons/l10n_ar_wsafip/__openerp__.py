55061cc6d411d602eeb929e7b317b8272be553b1
Cristian S. Rocha <cristian.rocha@moldeo.coop>
2016-01-25 18:44:00 -0300

    [REF] Tests working again

--- addons/l10n_ar_wsafip/__openerp__.py
+++ addons/l10n_ar_wsafip/__openerp__.py
@@ -1,45 +1,47 @@
 # -*- coding: utf-8 -*-
-{'active': False,
- 'author': 'OpenERP - Team de Localizaci\xc3\xb3n Argentina',
- 'category': 'Localization/Argentina',
- 'demo_xml': [],
- 'depends': ['crypto'],
- 'description': '''
- Configuración y API para acceder a las Web Services de la AFIP
-
- Incluye:
- - Wizard para instalar los claves para acceder a las Web Services.
- - API para realizar consultas en la Web Services desde OpenERP.
-
- El módulo l10n_ar_wsafip permite a OpenERP acceder a los servicios del AFIP a
- través de Web Services. Este módulo es un servicio para administradores y
- programadores, donde podrán configurar el servidor, la autentificación y además
- tendrán acceso a una API genérica en Python para utilizar los servicios AFIP.
-
- Para poder ejecutar los tests es necesario cargar la clave privada y el
- certificado al archivo test_key.yml.
-
- Tenga en cuenta que estas claves son personales y pueden traer conflicto
- publicarlas en los repositorios públicos.''',
- 'external_dependencies': {
-     'python': ['suds']
- },
- 'installable': True,
- 'license': 'AGPL-3',
- 'name': 'Argentina - Base para los Web Services del AFIP',
- 'demo': ['data/keys.yml',
-          'data/wsafip_service.yml'],
- 'test': ['test/test_mime_signer.yml',
-          'test/test_wsafip_service.yml'],
- 'data': ['data/wsafip_sequence.xml',
-          'data/wsafip_server.xml',
-          'data/wsafip_menuitem.xml',
-          'data/wsafip_connection_view.xml',
-          'data/wsafip_server_view.xml',
-          'data/wsafip_error_view.xml',
-          'data/wsafip_config.xml',
-          'security/wsafip_security.xml',
-          'security/ir.model.access.csv'],
- 'version': '8.0.1.1',
- 'website': 'https://launchpad.net/~openerp-l10n-ar-localization'}
+{
+    'name': 'Argentina - Base para los Web Services del AFIP',
+    'active': False,
+     'author': 'OpenERP - Team de Localizaci\xc3\xb3n Argentina',
+     'category': 'Localization/Argentina',
+     'demo_xml': [],
+     'depends': ['crypto'],
+     'description': '''
+     Configuración y API para acceder a las Web Services de la AFIP
+    
+     Incluye:
+     - Wizard para instalar los claves para acceder a las Web Services.
+     - API para realizar consultas en la Web Services desde OpenERP.
+    
+     El módulo l10n_ar_wsafip permite a OpenERP acceder a los servicios del AFIP a
+     través de Web Services. Este módulo es un servicio para administradores y
+     programadores, donde podrán configurar el servidor, la autentificación y además
+     tendrán acceso a una API genérica en Python para utilizar los servicios AFIP.
+    
+     Para poder ejecutar los tests es necesario cargar la clave privada y el
+     certificado al archivo test_key.yml.
+    
+     Tenga en cuenta que estas claves son personales y pueden traer conflicto
+     publicarlas en los repositorios públicos.''',
+     'external_dependencies': {
+         'python': ['suds']
+     },
+     'license': 'AGPL-3',
+     'data': ['data/wsafip_sequence.xml',
+              'data/wsafip_server.xml',
+              'data/wsafip_menuitem.xml',
+              'data/wsafip_connection_view.xml',
+              'data/wsafip_server_view.xml',
+              'data/wsafip_error_view.xml',
+              'data/wsafip_config.xml',
+              'security/wsafip_security.xml',
+              'security/ir.model.access.csv'],
+     'demo': ['data/keys.yml',
+              'data/wsafip_service.yml'],
+     'test': ['test/test_mime_signer.yml',
+              'test/test_wsafip_service.yml'],
+     'version': '8.0.2.1',
+     'website': 'https://launchpad.net/~openerp-l10n-ar-localization',
+     'installable': True,
+     }
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
